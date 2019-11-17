ada627c900a10963b7f4f4a3337ff43a13652016
Jerrico Gamis <jecklgamis@gmail.com>
2019-11-18 07:42:19 +1100

    Use ubuntu-18.04 for CouchDB

--- couchdb/run-couchdb.sh
+++ couchdb/run-couchdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-if test -f /etc/default/couchdb; then source /etc/default/couchdb; fi
 if test -f /lib/lsb/init-functions; then source /lib/lsb/init-functions; fi
-exec /usr/bin/couchdb
+exec /opt/couchdb/bin/couchdb
 
